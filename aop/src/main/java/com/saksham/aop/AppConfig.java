2f4dd1590cb9529d7dbc93666d407403d96e2632
Saksham-Bhardwaj <sakshambhardwaj002@gmail.com>
2020-06-05 04:04:03 +0530

    Getting started with AOP

---
+++ aop/src/main/java/com/saksham/aop/AppConfig.java
@@ -0,0 +1,9 @@
+package com.saksham.aop;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.saksham.aop")
+public class AppConfig {
+}
